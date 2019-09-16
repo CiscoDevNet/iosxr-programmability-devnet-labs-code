670c90b061205156216438f6d6073a55ffb1888c
dethanga <41729073+dethanga@users.noreply.github.com>
2019-09-16 11:09:07 +0530

    updated IP address

--- reset_setup/connection_details.sh
+++ reset_setup/connection_details.sh
@@ -3,9 +3,9 @@
 r1_user="admin"
 r1_password="admin"
 r1_port=2222
-r1_ip="10.10.20.170"
+r1_ip="10.10.20.70"
 
 r2_user="admin"
 r2_password="admin"
 r2_port=2232
-r2_ip="10.10.20.170"
+r2_ip="10.10.20.70"
